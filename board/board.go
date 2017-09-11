06d5d8099667c42dfb50a38d4d105b564be56aa2
Michael Aherne <micaherne@blueyonder.co.uk>
2017-09-11 22:43:22 +0100

    Move generation implemented (no legality tests)

--- board/board.go
+++ board/board.go
@@ -1,13 +1,10 @@
 package board
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"unicode"
 )
-
-type Piece int
 
 const (
 	EMPTY  = 0
@@ -21,39 +18,70 @@ const (
 
 const (
 	BLACK = 0
-	WHITE = 1
+	WHITE = 8 // Flag
+)
+
+const (
+	N   = 16
+	E   = 1
+	S   = -16
+	W   = -1
+	NE  = N + E // 17
+	SE  = S + E // -15
+	SW  = S + W // -17
+	NW  = N + W //15
+	NNE = N + NE
+	ENE = E + NE
+	ESE = E + SE
+	SSE = S + SE
+	SSW = S + SW
+	WSW = W + SW
+	WNW = W + NW
+	NNW = N + NW
+)
+
+var (
+	DIAGONALS         = []int{NE, SE, SW, NW}
+	LINES             = []int{N, S, E, W}
+	DIAGONALSANDLINES = append(DIAGONALS, LINES...)
+	SDIAGONALS        = []int{SE, SW}
+	NDIAGONALS        = []int{NE, NW}
+	KNIGHTMOVES       = []int{NNE, ENE, ESE, SSE, SSW, WSW, WNW, NNW}
 )
 
 type Board struct {
-	squares [128]Piece
+	squares     [128]int
+	whiteToMove bool
+	castling    int // KQkq
+	ep          int
 }
 
-func (p Piece) GetType() int {
-	return int(p) & 7
+func GetPieceType(p int) int {
+	return p & 7
 }
 
-func (p Piece) GetColour() int {
-	return int(p) >> 3 & 1
+func GetColour(p int) int {
+	return p & WHITE
 }
 
-func PieceFromNotation(symbol rune) Piece {
-	var result Piece
+func PieceFromNotation(symbol rune) int {
+	var result int
 	switch unicode.ToLower(symbol) {
 	case 'p':
-		result = Piece(PAWN)
+		result = PAWN
 	case 'n':
-		result = Piece(KNIGHT)
+		result = KNIGHT
 	case 'b':
-		result = Piece(BISHOP)
+		result = BISHOP
 	case 'r':
-		result = Piece(ROOK)
+		result = ROOK
 	case 'q':
-		result = Piece(QUEEN)
+		result = QUEEN
 	case 'k':
-		result = Piece(KING)
+		result = KING
 	}
 	if unicode.ToUpper(symbol) == symbol {
-		result |= 8
+		result |= WHITE
 	}
 	return result
 }
@@ -69,11 +97,216 @@ func FromFEN(fen string) Board {
 				file += n
 			} else {
 				b.squares[(7-rank)*16+file] = PieceFromNotation(char)
-				fmt.Printf("%d %d ", (7-rank)*16+file, PieceFromNotation(char))
 				file++
 			}
 		}
-		fmt.Println("")
 	}
+
+	b.whiteToMove = (fenParts[1] == "w")
+
+	castlingBits := map[string]int{
+		"K": 8,
+		"Q": 4,
+		"k": 2,
+		"q": 1,
+	}
+
+	for letter, bit := range castlingBits {
+		if strings.Contains(fenParts[2], letter) {
+			b.castling |= bit
+		}
+	}
+
+	if fenParts[3] == "-" {
+		b.ep = -1
+	} else {
+		b.ep = NotationToSquareIndex(fenParts[3])
+	}
+
 	return b
+}
+
+type Move struct {
+	from int
+	to   int
+}
+
+type MoveUndo struct {
+	from     int
+	to       int
+	captured int
+	ep       int
+	halfMove int
+	castling int
+}
+
+func GenerateMoves(b Board) []Move {
+	result := make([]Move, 0)
+	for i := 0; i < 128; i++ {
+		if b.squares[i] == EMPTY {
+			continue
+		}
+
+		if (b.whiteToMove && GetColour(b.squares[i]) == WHITE) || (!b.whiteToMove && GetColour(b.squares[i]) == BLACK) {
+			moves := GeneratePieceMoves(b, i)
+			result = append(result, moves...)
+		}
+	}
+	return result
+}
+
+func GeneratePieceMoves(b Board, i int) []Move {
+	piece := b.squares[i]
+	switch GetPieceType(piece) {
+	case PAWN:
+		if b.whiteToMove {
+			return GeneratePawnMoves(b, i, N, NDIAGONALS, i&0xF0 == 0x10)
+		} else {
+			return GeneratePawnMoves(b, i, S, SDIAGONALS, i&0xF0 == 0x60)
+		}
+	case KNIGHT:
+		return GenerateSingleMoves(b, i, KNIGHTMOVES)
+	case BISHOP:
+		return GenerateSlides(b, i, DIAGONALS)
+	case ROOK:
+		return GenerateSlides(b, i, LINES)
+	case QUEEN:
+		return GenerateSlides(b, i, DIAGONALSANDLINES)
+	case KING:
+		kingMoves := GenerateSingleMoves(b, i, DIAGONALSANDLINES)
+		var castleKingsideAllowed, castleQueensideAllowed bool
+		if b.whiteToMove {
+			castleKingsideAllowed = b.castling&8 == 8
+			castleQueensideAllowed = b.castling&4 == 4
+		} else {
+			castleKingsideAllowed = b.castling&2 == 2
+			castleQueensideAllowed = b.castling&1 == 1
+		}
+		if castleKingsideAllowed && CastlingLegal(b, i, E) {
+			kingMoves = append(kingMoves, Move{i, i + 2})
+		}
+		if castleQueensideAllowed && CastlingLegal(b, i, W) {
+			kingMoves = append(kingMoves, Move{i, i - 2})
+		}
+		return kingMoves
+	}
+
+	// TODO: Raise error
+	return make([]Move, 0)
+}
+
+func GenerateSingleMoves(b Board, i int, offsets []int) []Move {
+	result := make([]Move, 0)
+	ownColour := GetColour(b.squares[i])
+	for _, offset := range offsets {
+		if toSquare := i + offset; LegalSquareIndex(toSquare) {
+			if b.squares[toSquare] == 0 || GetColour(b.squares[toSquare]) != ownColour {
+				result = append(result, Move{i, toSquare})
+			}
+		}
+	}
+	return result
+}
+
+func GenerateSlides(b Board, i int, offsets []int) []Move {
+	result := make([]Move, 0)
+	ownColour := GetColour(b.squares[i])
+	for _, offset := range offsets {
+		for toSquare := i + offset; toSquare >= 0 && LegalSquareIndex(toSquare); toSquare += offset {
+			if b.squares[toSquare] == 0 {
+				result = append(result, Move{i, toSquare})
+			} else if GetColour(b.squares[toSquare]) != ownColour {
+				result = append(result, Move{i, toSquare})
+				break
+			} else {
+				break
+			}
+		}
+	}
+	return result
+}
+
+func GeneratePawnMoves(b Board, i int, forward int, captureOffsets []int, onHomeRank bool) []Move {
+	ownColour := GetColour(b.squares[i])
+	var result []Move
+	if onHomeRank {
+		result = GeneratePawnSlides(b, i, forward)
+	} else {
+		result = make([]Move, 0)
+		if toSquare := i + forward; toSquare&0x88 == 0 && b.squares[toSquare] == 0 {
+			result = append(result, Move{i, toSquare})
+		}
+	}
+
+	for _, offset := range captureOffsets {
+		toSquare := i + offset
+		if (toSquare)&0x88 > 0 {
+			continue
+		}
+		if b.ep == toSquare {
+			result = append(result, Move{i, toSquare})
+			continue
+		}
+		if b.squares[toSquare] == 0 {
+			continue
+		}
+		if GetColour(b.squares[toSquare]) == ownColour {
+			continue
+		}
+		result = append(result, Move{i, toSquare})
+	}
+	return result
+}
+
+// GeneratePawnSlides returns the valid non-capturing moves for
+// a pawn on the home rank. Behaviour is undefined for pawns on
+// any other rank.
+func GeneratePawnSlides(b Board, i int, offset int) []Move {
+	result := make([]Move, 0)
+	toSquare := i + offset
+	if b.squares[toSquare] == 0 {
+		result = append(result, Move{i, toSquare})
+		toSquare := i + offset
+		if b.squares[toSquare] == 0 {
+			result = append(result, Move{i, toSquare})
+		}
+	}
+
+	return result
+}
+
+func LegalSquareIndex(i int) bool {
+	return i >= 0 && i&0x88 == 0
+}
+
+// CastlingLegal checks that the correct squares are empty
+// and not attacked.
+func CastlingLegal(b Board, i int, direction int) bool {
+	var rookPosition int
+	switch direction {
+	case W:
+		if (b.whiteToMove && b.castling&4 == 0) || (!b.whiteToMove && b.castling&1 == 0) {
+			return false
+		}
+		rookPosition = i - 4
+	case E:
+		if (b.whiteToMove && b.castling&8 == 0) || (!b.whiteToMove && b.castling&2 == 0) {
+			return false
+		}
+		rookPosition = i + 3
+	default:
+		// TODO: Throw error
+		return false
+	}
+	for sq := i + direction; sq != rookPosition; sq += direction {
+		if b.squares[sq] != EMPTY {
+			return false
+		}
+	}
+	// TODO: Check attacks on intermediate squares
+	return true
+}
+
+func NotationToSquareIndex(notation string) int {
+	return int((notation[0] - "a"[0]) + (notation[1]-"1"[0])*16)
 }
--- board/board_test.go
+++ board/board_test.go
@@ -6,17 +6,32 @@ import (
 
 func TestBoardFromFEN(t *testing.T) {
 	b := FromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-	if b.squares[0].GetType() != ROOK+(WHITE*8) {
-		t.Error("Piece 0 should be white rook")
+	if b.squares[0] != WHITE|ROOK {
+		t.Error("Piece 0 should be white rook, not %s", GetPieceType(b.squares[0]))
+	}
+	if !b.whiteToMove {
+		t.Error("Should be white to move")
+	}
+	if b.castling != 0x0F {
+		t.Error("All castling should be available")
+	}
+	if b.ep != -1 {
+		t.Errorf("e.p. square should be -1, not %d", b.ep)
+	}
+
+	// Test e.p.
+	b2 := FromFEN("8/8/8/8/8/8/8/8 w KQkq c3 0 1")
+	if b2.ep != 0x22 {
+		t.Errorf("e.p. should be 0x22, not %X", b2.ep)
 	}
 }
 
-func TestGetType(t *testing.T) {
-	p := Piece((WHITE * 8) | KNIGHT)
-	if pt := p.GetType(); pt != KNIGHT {
+func TestGetPieceType(t *testing.T) {
+	p := WHITE | KNIGHT
+	if pt := GetPieceType(p); pt != KNIGHT {
 		t.Error("Piece should be a knight")
 	}
-	if pt := p.GetColour(); pt != WHITE {
+	if pt := GetColour(p); pt != WHITE {
 		t.Error("Piece should be white")
 	}
 }
@@ -25,4 +40,54 @@ func TestPieceFromNotation(t *testing.T) {
 	if p := PieceFromNotation('n'); p != BLACK|KNIGHT {
 		t.Error("n should be a black knight not %d", p)
 	}
+	if p := PieceFromNotation('R'); p != 8+ROOK {
+		t.Error("R should be a white rook")
+	}
+}
+
+func TestGenerateMoves(t *testing.T) {
+	b := FromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+	m := GenerateMoves(b)
+	if len(m) != 20 {
+		t.Errorf("Initial position should return 20 moves, not %d", len(m))
+	}
+
+	b.whiteToMove = false
+	m = GenerateMoves(b)
+	if len(m) != 20 {
+		t.Errorf("Initial position with black to move should return 20 moves, not %d", len(m))
+	}
+
+	b = FromFEN("7r/8/8/8/8/P7/8/4K2R w K - 0 1")
+	m = GeneratePieceMoves(b, 4)
+	if len(m) != 6 {
+		t.Errorf("Castling should be allowed", m)
+	}
+	m = GeneratePieceMoves(b, 32)
+	if len(m) != 1 {
+		t.Errorf("Pawn should have 1 move, not %d", len(m))
+	}
+
+	m = GeneratePieceMoves(b, 7)
+	if len(m) != 9 {
+		t.Errorf("Rook should have 9 moves, not %d", len(m))
+	}
+
+}
+
+func TestNotationToSquareIndex(t *testing.T) {
+	correct := map[string]int{
+		"a1": 0x00,
+		"a3": 0x20,
+		"d2": 0x13,
+		"h8": 0x77,
+	}
+
+	for notation, index := range correct {
+		i := NotationToSquareIndex(notation)
+		if i != index {
+			t.Errorf("%s should be index %d, not %d", notation, index, i)
+		}
+	}
+
 }
