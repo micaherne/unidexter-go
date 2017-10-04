b4c31b446150a90d59784f4f510db80c0929cb78
Michael Aherne <micaherne@blueyonder.co.uk>
2017-10-04 23:16:10 +0100

    Improved hashing tests

--- board/hash_test.go
+++ board/hash_test.go
@@ -14,28 +14,50 @@ func TestInitZobristKeys(t *testing.T) {
 		t.Errorf("key should be zero for empty piece")
 	}
 
-	b := FromFEN(InitialPositionFEN)
-	b.CalculateZobristHash()
-	if b.zobristKey == 0 {
-		t.Errorf("key should not be zero")
-	}
-	initialPosKey := b.zobristKey
+}
 
-	MakeMove(b, Move{0x14, 0x34, 0})
+func TestCalculateZobristHash(t *testing.T) {
 
-	b.CalculateZobristHash()
+	InitZobristKeys()
 
-	move2Key := b.zobristKey
-
-	if initialPosKey == move2Key {
-		t.Errorf("keys should not be equal")
+	correct := map[string][]Move{
+		InitialPositionFEN:           []Move{{0x14, 0x34, 0}},
+		"4k2r/8/8/8/8/8/8/4K3 b k -": []Move{{0x74, 0x76, 0}},
 	}
 
-	UndoMove(b)
+	for fen, moves := range correct {
+		b := FromFEN(fen)
+		b.CalculateZobristHash()
+		if b.zobristKey == 0 {
+			t.Errorf("key should not be zero %s %b", b, b.zobristKey)
+		}
+		initialPosKey := b.zobristKey
 
-	b.CalculateZobristHash()
-	if b.zobristKey != initialPosKey {
-		t.Errorf("keys should be equal after undo")
+		for _, move := range moves {
+			MakeMove(b, move)
+
+			b.CalculateZobristHash()
+
+			move2Key := b.zobristKey
+
+			if initialPosKey == move2Key {
+				t.Errorf("keys should not be equal")
+			}
+
+			UndoMove(b)
+
+			b.CalculateZobristHash()
+			if b.zobristKey != initialPosKey {
+				t.Errorf("keys should be equal after undo, %s %s", fen, move)
+			}
+		}
+
 	}
 
+}
+
+func BenchmarkInitZobristKeys(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		InitZobristKeys()
+	}
 }
